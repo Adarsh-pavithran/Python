55aa744ff7ff39402ce6d522e08fe7ef6516c067
unknown <adarshpavithran0707@gmail.com>
2026-09-10 10:53:28 +0530

    second commit

---
+++ Week 2/Day 1 Function argument/index.py
@@ -0,0 +1,99 @@
+#Function arguments
+
+# ----------------------------
+
+# 1) Required aragument / Positional argument
+
+# def add(a , b):
+#     print(a+b)
+
+# add(10 , 20)
+
+
+# 2 ) Default argumnet / assign  value to parameter
+
+# def add(a , b=40):
+#     print(a+b)
+
+# add(10 , )
+
+
+#3)  Keyword arguments
+
+# def students(name ,age):
+#     print(name,age)
+# students(name='adarsh' , age="22")
+
+
+# 4) Arbitary argument /Return Tuple ()
+
+# def add(a, *args):
+#     print(a ,args)
+# add(1,2,3,4,5,6)
+
+# def add(*args):
+#     #print(sum(args)) / Using default sum method
+#     total = 0
+#     for i in args:
+#         total += i
+#     return total
+# print(add(1,2,3,4,5))
+
+
+# 5) Arbitary  Keyword argument  / return dictionary {}
+
+# def student(**kwargs):
+#     print(kwargs)
+#     print(kwargs['age'])
+# student(name='adarsh' , age="22" , city="kannur")
+
+# --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+# Normal fuction
+
+# def square(x):
+#     return x* x
+# print(square(5))
+
+
+#  Lambda function
+
+# square = lambda x: x * x
+# print(square(5))
+
+
+# add = lambda a , b : a+b
+# print(add(20 , 30))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
---
+++ Week 2/Day 2 Comprehension/index.py
@@ -0,0 +1,15 @@
+#  1) Without comprehention
+
+# even_num =[]
+# for i in range(1 ,11):
+#     if i%2==0:
+#         even_num.append(i)
+
+# print (even_num)
+
+
+
+# 2 )With comprehention
+
+# even_num = [ i for i in range(1 , 11) if i%2==0 ]
+# print(even_num)
